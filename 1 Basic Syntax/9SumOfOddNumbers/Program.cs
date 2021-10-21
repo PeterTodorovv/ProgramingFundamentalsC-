2184ac392aa7e43f5f2e102c6f3304d1fd08ed7d
PeterTodorovv <petertodorovv@gmail.com>
2021-10-21 21:02:52 +0300

    First commit

---
+++ 1 Basic Syntax/9SumOfOddNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _9SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int total = 0;
+            for(int i = 1; i <= n*2; i+=2)
+            {
+                total += i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Sum: {total}");
+        }
+    }
+}
